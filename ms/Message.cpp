938668220cc24eb7e7739d1a0b6908098d4f6470
ms930602 <ms930602@sina.cn>
2018-07-10 11:23:52 +0800

    添加LUA初始化方法

--- ms/Message.cpp
+++ ms/Message.cpp
@@ -15,6 +15,16 @@ CMessage::~CMessage()
 {
 }
 
+CString CMessage::GetWindowTitle(HWND hwnd)
+{
+	CString CStitle;
+	::GetWindowText(hwnd, CStitle.GetBuffer(MAX_PATH), MAX_PATH);
+	CStitle.ReleaseBuffer();
+	CStitle = CStitle.Left(14);
+
+	return CStitle;
+}
+
 LRESULT CMessage::our_wndproc(HWND hWnd, UINT wMsg, WPARAM wParam, LPARAM lParam)
 {
 	//////////////////////////////////////////////////////////////////////////
@@ -92,4 +102,67 @@ void CMessage::un_subclass_game_wndproc()
 		dbgPrint(_T("卸载子类化窗口"));
 		::SetWindowLong(GamehWnd, GWL_WNDPROC, (LONG)funWndProc);
 	}
+}
+
+int CMessage::InitLuaFun()
+{
+	HMODULE LuaPlus = GetModuleHandleA("LuaPlus.dll");
+	if (LuaPlus)
+	{
+		GLua_Dostring = (Glua_dostring)GetProcAddress(LuaPlus, "lua_dostring");
+		GLua_Gettable = (Glua_gettable)GetProcAddress(LuaPlus, "lua_gettable");
+		GLua_Pushstring = (Glua_pushstring)GetProcAddress(LuaPlus, "lua_pushstring");
+		GLua_Tonumber = (Glua_tonumber)GetProcAddress(LuaPlus, "lua_tonumber");
+		GLua_Tostring = (Glua_tostring)GetProcAddress(LuaPlus, "lua_tostring");
+		GLua_Settop = (Glua_settop)GetProcAddress(LuaPlus, "lua_settop");
+		GpLua_Call = (Gplua_call)GetProcAddress(LuaPlus, "lua_pcall");
+		GLua_Settable = (Glua_settable)GetProcAddress(LuaPlus, "lua_settable");
+		GLua_Pushcclosure = (Glua_pushcclosure)GetProcAddress(LuaPlus, "lua_pushcclosure");
+		GLua_Loadfile = (Glua_loadfile)GetProcAddress(LuaPlus, "luaL_loadfile");
+		Gplua_type = (Glua_type)GetProcAddress(LuaPlus, "lua_type");
+		if (GLua_Dostring && GLua_Gettable && GLua_Pushstring
+			&& GLua_Tonumber && GLua_Tostring &&GLua_Settop)
+		{
+			return 1;//获取lua函数地址成功
+		}
+		else
+		{
+			return 0;//获取地址失败，模块句柄获取成功
+		}
+	}
+	return -1;//无效模块句柄
+}
+
+int CMessage::GetLuaState()
+{
+	int L = 0;
+	try
+	{
+		while (!L)
+		{
+			CString CStitle = GetWindowTitle(GamehWnd);
+			if (CStitle.Find(_T("《新天龙八部》"), 0) != -1)
+			{
+				DWORD LUASTATE_BASE = 0x0;
+				__asm
+				{
+					mov ecx, LUASTATE_BASE;
+					mov ecx, [ecx];
+					mov edx, [ecx];
+					call[edx + 0x3c];
+					mov eax, [eax];
+					mov  L, eax;
+				}
+				dbgPrint("L  %x", L);
+			}
+			Sleep(500);
+		}
+	}
+	catch (...)
+	{
+		L = -1;
+		dbgPrint(__FUNCTION__);
+	}
+
+	return L;
 }
--- ms/Message.h
+++ ms/Message.h
@@ -1,4 +1,18 @@
 #pragma once
+
+
+typedef int(*Glua_dostring)(int, const char *);//do_string
+typedef int(*Glua_pushstring)(int, const char *);//pushstring
+typedef void(*Glua_gettable)(int, int);//gettable
+typedef double(*Glua_tonumber)(int, double);//tonumber
+typedef int(*Glua_settop)(int, int);//settop
+typedef const char*(*Glua_tostring)(int, int);//tostring
+typedef int(*Glua_pushcclosure)(int, int, int);//
+typedef void(*Glua_settable)(int, int);
+typedef int(*Glua_loadfile)(int, const char*);
+typedef int(*Gplua_call)(int, int, int, int);
+typedef int(*Glua_type)(int, int);
+
 class CMessage
 {
 public:
@@ -8,6 +22,15 @@ public:
 	void Init() {//初始化游戏lua函数，lua状态机，子类化游戏窗口过程
 		GetGameWindow();//获取窗口句柄
 		subclass_game_wndproc();//子类化窗口过程
+
+		wchar_t lpBuffer[MAX_PATH] = { 0 };
+		GetSystemDirectory(lpBuffer, MAX_PATH);
+		CString strPAth(lpBuffer);
+		CString strPAth2;
+		strPAth2.Format(_T("\\Client%x.cfg"), GetCurrentProcessId());
+		strPAth += strPAth2;
+
+		TRACE(strPAth);
 	};
 	void Release() {//卸载子类化
 		un_subclass_game_wndproc();
@@ -15,11 +38,31 @@ public:
 public:
 	HWND GamehWnd = nullptr;//游戏的窗口句柄
 	WNDPROC funWndProc = nullptr;
+
+	CString GetWindowTitle(HWND hwnd);//获取天龙标题前的文字
 private:
 	static LRESULT CALLBACK our_wndproc(HWND hWnd, UINT wMsg, WPARAM wParam, LPARAM lParam);//窗口消息钩子
+private:
+	/************************************************************************/
+	/* 游戏的lua函数指针定义                                                                     */
+	/************************************************************************/
+	Glua_dostring GLua_Dostring = nullptr;
+	Glua_pushstring GLua_Pushstring = nullptr;
+	Glua_gettable GLua_Gettable = nullptr;
+	Glua_tonumber GLua_Tonumber = nullptr;
+	Glua_settop   GLua_Settop = nullptr;
+	Glua_tostring GLua_Tostring = nullptr;
+	Glua_pushcclosure GLua_Pushcclosure = nullptr;
+	Glua_settable	GLua_Settable = nullptr;
+	Glua_loadfile	GLua_Loadfile = nullptr;
+	Gplua_call	  GpLua_Call = nullptr;
+	Glua_type   Gplua_type = nullptr;
+	int lua_state = 0;//lua状态机指针
 private:
 	HWND GetGameWindow();
 	CString MyGetClassName(HWND hwnd);
 	void subclass_game_wndproc();//子类化窗口过程函数
 	void un_subclass_game_wndproc();	//卸载子类化窗口过程
+	int InitLuaFun();//获取lua库函数
+	int GetLuaState();//获取游戏中lua状态指针
 };
